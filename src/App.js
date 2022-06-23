d2342a26336db9c794d265479fecc99211ea2434
Raymon Zhang <raymonzhang20@gmail.com>
2022-06-22 19:00:46 -0700

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXOR82sAF64xL44BP7mkl6FMcyxU86f-c",
+  authDomain: "tether-2781a.firebaseapp.com",
+  projectId: "tether-2781a",
+  storageBucket: "tether-2781a.appspot.com",
+  messagingSenderId: "785492280537",
+  appId: "1:785492280537:web:8b7fa1a5346491b6f86ac8",
+  measurementId: "G-37B2ZG3ENB"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
